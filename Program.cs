63fc0a03c580bdd1641b30e402c14224fd4d0d2e
Cysia15 <mixu12anaix8@gmail.com>
2024-10-29 08:30:14 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+
+class Kalkulator
+{
+    static void Main(string[] args)
+    {
+       
+        int liczba1p, liczba2p;
+        char operatora;
+        int operatorb;
+        
+            Console.WriteLine("podaj pierwsza liczbe");
+            bool liczba1 = int.TryParse(Console.ReadLine(), out liczba1p);
+            Console.WriteLine("podaj operator(+,-,*,/)");
+            bool op = char.TryParse(Console.ReadLine(), out operatora);
+
+            Console.WriteLine("podaj druga liczbe");
+            bool liczba2 = int.TryParse(Console.ReadLine(), out liczba2p);
+        switch (operatora)
+        {
+            case '+':
+            {
+                    Console.WriteLine(liczba1p + liczba2p);
+                    break;
+            }
+            case '-':
+                {
+                    Console.WriteLine(liczba1p - liczba2p);
+                    break;
+                }
+            case '*':
+                {
+                    Console.WriteLine(liczba1p * liczba2p);
+                    break;
+                }
+            case '/':
+                {
+                    Console.WriteLine(liczba1p / liczba2p);
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
+       
+        
+    }
+    
+}
